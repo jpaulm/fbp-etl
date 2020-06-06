3b99a7d2e0880e0ecc09522022371619db209dc9
Paul Morrison <jpaulmorr@gmail.com>
2020-06-05 22:31:21 -0400

    Removed ReadJDBC println statements

--- Step08/code/components/ReadJDBC.java
+++ Step08/code/components/ReadJDBC.java
@@ -5,6 +5,7 @@ import java.sql.*;
 import com.jpaulmorrison.fbp.core.engine.Component;  // Using 'Connection', 'Statement' and 'ResultSet' classes in java.sql package
 import com.jpaulmorrison.fbp.core.engine.ComponentDescription;
 import com.jpaulmorrison.fbp.core.engine.InPort;
+import com.jpaulmorrison.fbp.core.engine.InPorts;
 import com.jpaulmorrison.fbp.core.engine.InputPort;
 import com.jpaulmorrison.fbp.core.engine.OutPort;
 import com.jpaulmorrison.fbp.core.engine.OutputPort;
@@ -12,11 +13,10 @@ import com.jpaulmorrison.fbp.core.engine.Packet;
 import com.jpaulmorrison.jdbcstuff.resources.layouts.*;
 
 @ComponentDescription("Reads table from MySQL and outputs result")
-@InPorts({
+@InPorts({ 
 @InPort(value = "DATABASE", description = "Database name", type = String.class),
 @InPort(value = "USER", description = "User name", type = String.class),
 @InPort(value = "PSWD", description = "Password obtained from file", type = String.class)})
-
 @OutPort(value = "OUT", description = "Table rows", type = String.class)
 
 public class ReadJDBC extends Component {
@@ -64,13 +64,15 @@ public class ReadJDBC extends Component {
 			      ) {
 			         // Step 3: Execute a SQL SELECT query. The query result is returned in a 'ResultSet' object.
 			         String strSelect = "select title, author, price, qty from books";
-			         System.out.println("The SQL statement is: \"" + strSelect + "\"\n"); // Echo For debugging
+			         //System.out.println("The SQL statement is: \"" + strSelect + "\"\n"); // Echo For debugging
+			         outPort.send(create("The SQL statement is: \"" + strSelect + "\"\n")); // Echo For debugging
 			 
 			         ResultSet rset = stmt.executeQuery(strSelect);
 			 
 			         // Step 4: Process the ResultSet by scrolling the cursor forward via next().
 			         //  For each row, retrieve the contents of the cells with getXxx(columnName).
-			         System.out.println("The records selected are:");
+			         //System.out.println("The records selected are:");
+			         outPort.send(create("The records selected are:"));
 			         int rowCount = 0;
 			         while(rset.next()) {   // Move the cursor to the next row, return false if no more row
 			        	Book book = new Book(); 
@@ -83,7 +85,8 @@ public class ReadJDBC extends Component {
 			            outPort.send(create(outStr));
 			            ++rowCount;
 			         }
-			         System.out.println("Total number of records = " + rowCount);
+			         //System.out.println("Total number of records = " + rowCount);
+			         outPort.send(create("Total number of records = " + rowCount));
 			 
 			      } catch(SQLException ex) {
 			         ex.printStackTrace();

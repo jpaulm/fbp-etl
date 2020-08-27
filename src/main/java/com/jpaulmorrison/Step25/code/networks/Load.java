73d06e1f1798048c20c47b4b938ece79aaecacdc
Paul Morrison <jpaulmorr@gmail.com>
2020-08-26 20:05:13 -0400

    Java network names have been changed to remove minuses.﻿

--- src/main/java/com/jpaulmorrison/Step25/code/networks/Load.java
+++ src/main/java/com/jpaulmorrison/Step25/code/networks/Load.java
@@ -1,11 +1,10 @@
-package com.jpaulmorrison.Step25.code.networks;
-
+package com.jpaulmorrison.Step25.code.networks; //change package name, or delete statement, if desired
 import com.jpaulmorrison.fbp.core.engine.*; 
-@ComponentDescription("Load") 
+@ComponentDescription("Load.drw") 
 @InPort("IN")
 
 public class Load extends SubNet {
-String description = "Load";
+String description = "Load.drw";
 protected void define() { 
   component("WriteJDBC",com.jpaulmorrison.fbp.core.components.jdbc.WriteJDBC.class); 
   component("Read__pswd",com.jpaulmorrison.fbp.core.components.io.ReadFile.class); 
@@ -16,14 +15,7 @@ protected void define() {
   initialize("jdbc:mysql://localhost:3306/ebookshop!sales", component("WriteJDBC"), port("DATABASE")); 
   initialize("root", component("WriteJDBC"), port("USER")); 
   initialize("com.jpaulmorrison.jdbcstuff.resources.layouts.Sale", component("WriteJDBC"), port("CLASS")); 
+  initialize("[{\"colName\": \"id\",    \"objField\": \"id2\"},   {\"colName\": \"title\",    \"objField\": \"title2\"},   {\"colName\": \"author\",    \"objField\": \"author2\"},   {\"colName\": \"price\",    \"objField\": \"price2\"},   {\"colName\": \"qty\",    \"objField\": \"qty2\"} ,   {\"colName\": \"total\",    \"objField\": \"total2\"}  ]", component("WriteJDBC"), port("FIELDS")); 
   connect(component("SUBIN"), port("OUT"), component("WriteJDBC"), port("IN")); 
-  initialize("[{\"colName\": \"id\",    \"objField\": \"id2\"},"
-  		+ "   {\"colName\": \"title\",    \"objField\": \"title2\"},"
-  		+ "   {\"colName\": \"author\",    \"objField\": \"author2\"},"
-  		+ "   {\"colName\": \"price\",    \"objField\": \"price2\"}, "
-  		+ "   {\"colName\": \"total\",    \"objField\": \"total2\"},"
-  		+ "  {\"colName\": \"qty\",    \"objField\": \"qty2\"} ]",
-  		component("WriteJDBC"), port("FIELDS")); 
-  
 } 
 }
